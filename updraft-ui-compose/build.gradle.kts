ff1933f8c88111f60ac304f513aa15511e16c6cb
Michel Utke <utke.michel@gmail.com>
2026-07-24 17:58:20 +0200

    Migrate :updraft-ui-compose to com.android.kotlin.multiplatform.library with androidResources enabled

--- updraft-ui-compose/build.gradle.kts
+++ updraft-ui-compose/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.loco)
@@ -28,9 +28,15 @@ Loco {
 }
 
 kotlin {
-    androidTarget {
+    androidLibrary {
+        namespace = "com.appswithlove.updraft.ui"
+        compileSdk = 36
+        minSdk = 23
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
+        }
+        androidResources {
+            enable = true
         }
     }
 
@@ -64,18 +70,6 @@ kotlin {
 compose.resources {
     packageOfResClass = "com.appswithlove.updraft.ui.resources"
     publicResClass = false
-}
-
-android {
-    namespace = "com.appswithlove.updraft.ui"
-    compileSdk = 36
-    defaultConfig {
-        minSdk = 23
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
 }
 
 mavenPublishing {
